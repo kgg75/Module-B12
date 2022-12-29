150ecd4106f8b6a99b8902f33ab2900a7221a717
kgg75 <kgg75@mail.ru>
2022-12-29 21:20:29 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-bool IsKPeriodic(const std::string _str, const int _k) {    // _k  - кратность строки
+bool IsKPeriodic(const std::string _str, const int _k) {    // _k  - РєСЂР°С‚РЅРѕСЃС‚СЊ СЃС‚СЂРѕРєРё
     if (_k <= 0)
         return false;
 
@@ -21,36 +21,15 @@ bool IsKPeriodic(const std::string _str, const int _k) {    // _k  - кратность с
 
 int main() {
     system("chcp 1251");	// setlocale (LC_ALL,"");
-
-    //const std::string txt { "ABABDABACDABABCABAB" };
-    //const std::string pat { "ABABCABAB" };
-
-    //KMPSearch(pat, txt);
-    //int test1[]{ 1, 4, 20, 3, 10, 5 };
-    //const int test1[] { 1, 5, 3, 6, 5, 9 };
- 
-    //int test1[]{ 1, 4, 0, 0, 3, 10, 5 };
-    //const int value = 7;   //6; //
-    
-    //int test1[]{ 1, 4 };
-    //const int value = 7;   //6; //
-
-    //int test1[]{ 6, 9, 3, 0, 5, 4 };
-    //const int value = 18;   //6; //
-
-    //subArraySum(&test1[0], 2, value);
-    
     
     const std::string txt {"abcdabcdabcdabcd"};
     //const std::string txt{ "aaaaaaaaaa" };
     cout << txt << '\n';
     
-    int len = txt.size() / 2;   // длина для теста; далее половины тсроки тестировать нет смысла
+    int len = txt.size() / 2;   // РґР»РёРЅР° РґР»СЏ С‚РµСЃС‚Р°; РґР°Р»РµРµ РїРѕР»РѕРІРёРЅС‹ С‚СЃСЂРѕРєРё С‚РµСЃС‚РёСЂРѕРІР°С‚СЊ РЅРµС‚ СЃРјС‹СЃР»Р°
 
-    for (int i = -1; i <= len; i++) {
-        //KMPSearch(txt.substr(0, i), txt);
-        cout << "K = " << i << "; result = " << (IsKPeriodic(txt, i) ? "истина" : "ложь") << '\n';
-    }
+    for (int i = -1; i <= len; i++)
+        cout << "K = " << i << "; result = " << (IsKPeriodic(txt, i) ? "РёСЃС‚РёРЅР°" : "Р»РѕР¶СЊ") << '\n';
 
     return 0;
 }
